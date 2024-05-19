d7862a6bd176fcdd58d4a8884780289f3ff5bf49
Bhargav Gudapati <bhargav_gudapati@yahoo.com>
2024-05-19 08:20:35 -0400

    added support for taking in program file location as a command line argument

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,33 @@
 
 //! This is an emulator for the CHIP 8, written in Rust.
 
+use std::env;
 use std::fs::File;
-use std::io;
 use chip_8_emulator::chip_8::{drivers, TimedRunner};
 
 /// This is the main function for the emulatort
 fn main() {
     println!("Starting emulator");
-
-    let mut input_file_name = String::new();
-    println!("input the program location...\t");
-    let stdin = io::stdin();
-    let _ = stdin.read_line(&mut input_file_name);
-    let _ = input_file_name.remove(input_file_name.len() - 1);
+    let args = env::args().collect::<Vec<String>>();
+    let input_file_name = {
+	if args.len() < 2 {
+	    panic!("program file location not provided");
+	} else {
+	    &args[1]
+	}
+    };
     
+    println!("taking input program location from provided 1st argument");
     let program_file = File::open(input_file_name).expect("unable to find the program file specified");
 
+    println!("initializing chip8 decoder");
     let mut chip_8_system = TimedRunner::new(drivers::TerminalNumbers, drivers::TerminalBeep, drivers::KeySender::new());
+    
+    println!("loading program...");
     chip_8_system.init(program_file);
 
-    //let mut throwaway = String::new();
+    println!("starting decode loop");
     loop {
-	//let _ = stdin.read_line(&mut throwaway);
 	chip_8_system.decode_next_timed(1f64);
     }
 }
